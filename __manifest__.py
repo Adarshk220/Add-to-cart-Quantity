35641649e73561f14201281f7a0365d5011df87e
Adarsh <adarsh808681@gmail.com>
2024-04-30 14:23:22 +0530

    Add to cart Quantity

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Add to cart Quantity',
+    'author': 'Adarsh',
+    'version': '17.0.1.0.0',
+    'summary': 'Add to cart Quantity To The Shop Page',
+    'depends': ['base', 'website'],
+    'data': [
+        'views/add_to_cart_quantity_views.xml',
+    ],
+    'sequence': -101,
+    'application': True,
+}
